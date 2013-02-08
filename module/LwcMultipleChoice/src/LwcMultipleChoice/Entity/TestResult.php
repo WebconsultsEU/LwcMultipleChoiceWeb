fc2b0b72a0ff3fc1c7b1bdc9f8d6d5958731477b
John Behrens <john.behrens@webconsults.eu>
2013-02-08 15:30:01 +0100

    Test Result Handling

---
+++ module/LwcMultipleChoice/src/LwcMultipleChoice/Entity/TestResult.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace LwcMultipleChoice\Entity;
+
+/**
+ * Description of TestResult
+ *
+ * @author John Behrens <John.behrens@WebConsults.eu>
+ */
+class TestResult {
+    
+    
+    private $pointsMissed;
+    private $pointsScored;
+    
+    private $questionsTotal;
+   
+    public function getPointsMissed() {
+        return $this->pointsMissed;
+    }
+
+    public function setPointsMissed($pointsMissed) {
+        $this->pointsMissed = $pointsMissed;
+    }
+
+    public function getPointsScored() {
+        return $this->pointsScored;
+    }
+
+    public function setPointsScored($pointsScored) {
+        $this->pointsScored = $pointsScored;
+    }
+
+    public function getQuestionsTotal() {
+        return $this->questionsTotal;
+    }
+
+    public function setQuestionsTotal($questionsTotal) {
+        $this->questionsTotal = $questionsTotal;
+    }
+    
+    public function addPointsScored($points) {
+        $this->setPointsScored($this->getPointsScored() + $points);
+    }
+    public function addPointsMissed($points) {
+        $this->setPointsScored($this->getPointsScored() + $points);
+    }
+
+
+}
+
+?>
---
+++ module/LwcMultipleChoice/src/LwcMultipleChoice/Repository/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Test Repository 
+ */
+
+/**
+ * Description of Test
+ *
+ * @author John Behrens <John.behrens@WebConsults.eu>
+ */
+class Test {
+    //put your code here
+}
+
+?>
